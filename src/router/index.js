8ca607973b1decff5bf5e1e9e023674e103f0476
xiaofeng <760811650@qq.com>
2020-12-18 15:02:08 +0800

    登录首页 配置vue.config

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,13 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHashHistory  } from "vue-router";
+import Login from "../views/account/Login.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Login",
+    component: Login
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  
 ];
 
 const router = createRouter({
---
+++ src/views/account/Login.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="account">
+    <div class="form_wrap">
+       <a-form
+          ref="ruleForm"
+          v-bind="FormConfig.layout"
+        >
+          <a-form-item >
+            <label>用户名</label>
+            <a-input type="text"  />
+          </a-form-item>
+
+           <a-form-item >
+            <label>密码</label>
+            <a-input type="password"  />
+          </a-form-item>
+          <a-form-item>
+            <div id="captcha-dom" class="nc-container"></div>
+            <!--No-Captcha渲染的位置，其中 class 中必须包含 nc-container-->
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" html-type="submit" block>
+              登录
+            </a-button>
+          </a-form-item>
+           <a-form-item>
+            <div class="fs-12 text-center color-white" >
+              <a href="" class="color-white">忘记密码</a> |
+              <a href="" class="color-white">注册</a> 
+            </div>
+          </a-form-item>
+          
+        </a-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import {reactive, onMounted} from 'vue'
+import './captcha.js'
+export default {
+  name: "Login",
+  setup(props) {
+     const FormConfig = reactive({
+       layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 20 },
+      }
+     })
+
+      onMounted(() => {
+        var nc_token = ["FFFF0N00000000009931", (new Date()).getTime(), Math.random()].join(':');  // 没有做任何的响应
+        var NC_Opt = 
+        {
+            renderTo: "#captcha-dom",
+            appkey: "FFFF0N00000000009931",
+            scene: "nc_login",
+            token: nc_token,
+            customWidth: 300,
+            trans:{"key1":"code0"},
+            elementID: ["usernameID"],
+            is_Opt: 0,
+            language: "cn",
+            isEnabled: true,
+            timeout: 3000,
+            times:5,
+            apimap: {
+                // 'analyze': '//a.com/nocaptcha/analyze.jsonp',
+                // 'get_captcha': '//b.com/get_captcha/ver3',
+                // 'get_captcha': '//pin3.aliyun.com/get_captcha/ver3'
+                // 'get_img': '//c.com/get_img',
+                // 'checkcode': '//d.com/captcha/checkcode.jsonp',
+                // 'umid_Url': '//e.com/security/umscript/3.2.1/um.js',
+                // 'uab_Url': '//aeu.alicdn.com/js/uac/909.js',
+                // 'umid_serUrl': 'https://g.com/service/um.json'
+            },   
+            callback: function (data) { 
+                window.console && console.log(nc_token)
+                window.console && console.log(data.csessionid)
+                window.console && console.log(data.sig)
+            }
+        }
+        var nc = new noCaptcha(NC_Opt)
+        nc.upLang('cn', {
+            _startTEXT: "请按住滑块，拖动到最右边",
+            _yesTEXT: "验证通过",
+            _error300: "哎呀，出错了，点击<a href=\"javascript:__nc.reset()\">刷新</a>再来一次",
+            _errorNetwork: "网络不给力，请<a href=\"javascript:__nc.reset()\">点击刷新</a>",
+        })
+    })
+
+     return {
+       FormConfig
+     }
+  }
+};
+</script>
+
+<style lang="scss">
+  .account{
+    width: 100vw;
+    height: 100vh;
+    background-color: #282828;
+    .form_wrap{
+         width: 330px;
+         margin: auto;
+         padding-top:300px; 
+         color: #fff;
+         label{
+           font-size: 14px;
+           color: #fff;
+         }
+    }
+  }
+</style>
