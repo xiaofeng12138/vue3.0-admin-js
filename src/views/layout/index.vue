75c2c06adabf5f98ced45f970babea89f10272f0
xiaofeng <760811650@qq.com>
2020-12-23 15:59:05 +0800

    测试

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -10,6 +10,7 @@
         <a-layout-content
             :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
         >
+        
            <Content />
         </a-layout-content>
         </a-layout>
